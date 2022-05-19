956db8aaab4245c65ee164100d0c84ccddc31b06
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-05-20 00:12:16 +0200

    created function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = (word, times) => {
+  if (times < 0) return "ERROR";
+  let processed = '';
+  for (let i = 1; i <= times; i++) {
+    processed += word
+  }
+  return processed
 };
 
 // Do not edit below this line
