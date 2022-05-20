b01f8305e9534292704b356c47f7fcaebc944fb6
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-05-20 19:37:51 +0200

    defined 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (typeof x !== "number" || typeof y !== "number" || x < 0 || y < 0)
+    return "ERROR";
 
+  let sum = 0;
+  for (let i = Math.min(x, y); i <= Math.max(x, y); i++) sum += i;
+
+  return sum;
 };
 
 // Do not edit below this line
