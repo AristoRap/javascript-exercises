970ba9d1f8c25d5bf5a2210bab4a93994e2a5919
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-05-20 22:38:00 +0200

    defined 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  const currYear = (new Date).getFullYear();
+  arr.sort((a, b) => {
+    ageA = (a.yearOfDeath || currYear) - a.yearOfBirth
+    ageB = (b.yearOfDeath || currYear) - b.yearOfBirth
+    return ageA < ageB ? 1 : -1
+  })
+  return arr[0]
 };
 
 // Do not edit below this line
