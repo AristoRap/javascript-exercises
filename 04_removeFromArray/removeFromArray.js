c9c660203c6dfe1ce55f7ef909bf873a58dd24bf
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-05-20 19:29:06 +0200

    defined 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  for (let i = 1; i < arguments.length; i++) {
+    let index = arr.indexOf(arguments[i]);
+    index !== -1 ? arr.splice(index, 1) : null
+  }
+  return arr
 };
 
 // Do not edit below this line
