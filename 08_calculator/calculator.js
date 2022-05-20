bc28eed737006d3cb12ee7735bc34e501f7b16b0
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-05-20 20:49:14 +0200

    defined 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return +x + +y
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(x, y) {
+  return +x - +y
 
 };
 
-const power = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, num) => sum += num, 0)
 };
 
-const factorial = function() {
-	
+const multiply = function(arr) {
+  return arr.reduce((product, num) => (product *= num));
+};
+
+const power = function(x, y) {
+  return x ** y
+
+};
+
+const factorial = function(num) {
+  let product = 1
+  for (let i = 1; i <= num; i++) {
+    product *= i
+  }
+  return product
 };
 
 // Do not edit below this line
