caf78cccf3bff82a7f9bff2022321e7c8da17c9f
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-05-20 21:32:39 +0200

    defined 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  if (input < 0) return 'OOPS'
 
+  let fib = [1, 1]
+  for (let i = 2; i <= +input; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib[+input - 1]
 };
 
 // Do not edit below this line
